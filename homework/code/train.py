ed151bbd783a3951da68bc7956178e6792ff3953
happy-join-github <123853776+happy-join-github@users.noreply.github.com>
2024-06-07 11:28:45 +0800

    Delete homework directory

--- homework/code/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8.yaml")
-model = YOLO("yolov8n.pt")
-model = YOLO("yolov8n.yaml").load("yolov8n.pt")
-
-results = model.train(data="../dataset/data.yaml", epochs=100)
